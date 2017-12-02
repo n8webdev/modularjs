f3efeb033c10f726adcbb86baa82818c520a59f4
Nathan Torquato <n8webdev@gmail.com>
2017-12-02 17:55:57 -0200

    Object literal - initiate the module

--- object-literal/app.js
+++ object-literal/app.js
@@ -1,16 +1,49 @@
-var people = [];
-var template = $("#people-template").html();
-
-// add people
-$("#peopleModule").find("button").on("click", function() {
-  people.push($("#peopleModule").find("input").val());
-  $("#peopleModule").find("input").val('');
-  // data for Mustache Template
-  var data = {
-    people: people
+// IIFE - Immediately Invoked Function Expression - check readme #1
+(function(){
+  // object literal module
+  var people = {
+    people: ['Nathan', 'Lari'],
+    // starts the module
+    init: function() {
+      this.cacheDom();
+      this.render();
+    },
+    // caches the Dom and optimizes its use
+    cacheDom: function() {
+      this.$module  = $("#peopleModule");
+      this.$button  = this.$module.find("button");
+      this.$input   = this.$module.find("input");
+      this.$ul      = this.$module.find("ul");
+      this.template = this.$module.find("#people-template").html();
+    },
+    // use MustacheJS template engine
+    render: function() {
+      // data for Mustache template
+      var data = {
+        people: this.people
+      };
+      // runs template engine
+      this.$ul.html(Mustache.render(this.template, data));
+    }
   };
-  $("#peopleModule").find("ul").html(Mustache.render(template, data));
-});
+
+  people.init();
+
+})();
+
+// var people = [];
+// var template = $("#people-template").html();
+
+// // add people
+// $("#peopleModule").find("button").on("click", function() {
+//   people.push($("#peopleModule").find("input").val());
+//   $("#peopleModule").find("input").val('');
+//   // data for Mustache Template
+//   var data = {
+//     people: people
+//   };
+//   $("#peopleModule").find("ul").html(Mustache.render(template, data));
+// });
 
 // remove people
 $("#peopleModule").find("ul").delegate("i.del", "click", function(e) {
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Modular JavaScript
 
-Tutorial from learncode.academy on youtube.
+Tutorial from [learncode.academy](https://www.youtube.com/watch?v=HkFlM73G-hk&list=PLoYCgNOIyGABs-wDaaxChu82q_xQgUb4f&index=1) on youtube.
+
+## Tips
+* #1 - [IIFE](http://gregfranko.com/blog/i-love-my-iife/);
